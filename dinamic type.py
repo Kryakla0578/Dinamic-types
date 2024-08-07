5a8f45aac120b3c4f2905c47d3430e6da72d6294
Kryakla0578 <167692954+Kryakla0578@users.noreply.github.com>
2024-08-07 23:27:51 +0300

    Add files via upload

---
+++ dinamic type.py
@@ -0,0 +1,10 @@
+name = "Кирилл"
+print("Name:",name, type(name))
+age = 24
+print('Age:',age,type(age))
+age = (age+1)
+print('New age:',age,type(age))
+is_student = True
+print('Is student:', is_student, type(is_student))
+
+
